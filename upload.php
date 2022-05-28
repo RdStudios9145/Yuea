5459f04eee2ce3912bf601f9ab9f37d71fca0156
RdStudios <77551723+RdStudios9145@users.noreply.github.com>
2022-05-28 15:21:03 -0700

    Formatted upload.php

--- upload.php
+++ upload.php
@@ -2,47 +2,41 @@
 include ('./includes/header.php');
 
 if (isset($_FILES['video'])) {
-  $title = $_POST['video_title'];
-  $desc = $_POST['video_description'];
-  $keywords = $_POST['video_keywords'];
-  $privacy = @$_POST['privacy'];
-  $channel = $_POST['channel'];
-  if (!empty($title) || ($desc) || ($keywords) || ($privacy) || ($channel)) {
-    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-   $video_id = substr(str_shuffle($chars), 0, 15);
-   $video_id = md5($video_id);
-  }
-  else
-  {
-   die('empty fields');
-  }
-   if (($_FILES['video']['type']=='video/mp4')) {
-   $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-   $random_directory = substr(str_shuffle($chars), 0, 15);
+	$title = $_POST['video_title'];
+	$desc = $_POST['video_description'];
+	$keywords = $_POST['video_keywords'];
+	$privacy = @$_POST['privacy'];
+	$channel = $_POST['channel'];
+	if (!empty($title) || ($desc) || ($keywords) || ($privacy) || ($channel)) {
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$video_id = substr(str_shuffle($chars), 0, 15);
+		$video_id = md5($video_id);
+	} else {
+		die('empty fields');
+	}
+	if (($_FILES['video']['type']=='video/mp4')) {
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$random_directory = substr(str_shuffle($chars), 0, 15);
 
-   if (file_exists('data/channels/videos/' . $random_directory . ''.$_FILES['video']['name'])) {
-     echo 'video exists';
-   }
-   else
-   {
-
-   move_uploaded_file($_FILES['video']['tmp_name'],'data/channels/videos/' . $random_directory . ''.$_FILES['video']['name']);
-   $img_name = $_FILES['video']['name'];
-   $filename = "data/channels/videos/".$random_directory.$_FILES['video']['name'];
-   $md5_file = md5_file($filename);
-   $check_md5 = DB::query("SELECT file_md5 FROM videos WHERE file_md5='$md5_file'");
-   if (count($check_md5) != 0) {
-     unlink($filename);
-    die("This is a duplicate upload");
-   }else {
-     $date = date("Y F j");
-   $insert = DB::query("INSERT INTO videos VALUES ('','$title','$desc','$keywords','$user','$channel','$privacy','$date','0','$video_id','','$filename','images/thumbnail.png','no')");
-   DB::query("UPDATE videos SET file_md5='$md5_file' WHERE video_id='$video_id'");
-   die('The video was uploaded successfully');
-   }
-   }
-   }
-
+		if (file_exists('data/channels/videos/' . $random_directory . ''.$_FILES['video']['name'])) {
+			echo 'video exists';
+		} else {
+			move_uploaded_file($_FILES['video']['tmp_name'],'data/channels/videos/' . $random_directory . ''.$_FILES['video']['name']);
+			$img_name = $_FILES['video']['name'];
+			$filename = "data/channels/videos/".$random_directory.$_FILES['video']['name'];
+			$md5_file = md5_file($filename);
+			$check_md5 = DB::query("SELECT file_md5 FROM videos WHERE file_md5='$md5_file'");
+			if (count($check_md5) != 0) {
+				unlink($filename);
+				die("This is a duplicate upload");
+			} else {
+				$date = date("Y F j");
+				$insert = DB::query("INSERT INTO videos VALUES ('','$title','$desc','$keywords','$user','$channel','$privacy','$date','0','$video_id','','$filename','images/thumbnail.png','no')");
+				DB::query("UPDATE videos SET file_md5='$md5_file' WHERE video_id='$video_id'");
+				die('The video was uploaded successfully');
+			}
+		}
+	}
 }
 ?>
 <h2>Upload a Video</h2>
